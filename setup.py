d166fc71158220e8020beab703053e1ee8506a1d
BHEESETTIANAND <anandbheesetti@gmail.com>
2024-12-26 12:11:23 +0530

    setup and requirements files added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Ml pipeline",
+    version="0.0.0",
+    author="anand",
+    author_email="anandbheesetti@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
